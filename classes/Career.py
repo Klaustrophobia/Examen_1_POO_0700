acc7e3cd69d83697b37872c139d8275c0bb6d5d7
Klaustrophobia <Lalosempai2@gmail.com>
2023-02-25 21:31:45 -0600

    ACT II

--- classes/Career.py
+++ classes/Career.py
@@ -15,3 +15,18 @@ class Career:
     def get_courses(self):
         return self.carrera
     
+    @staticmethod
+    def find_all(db):
+        careers = []
+        for c in db.careers.find():
+            careers.append(Career(c["name"]))
+        return careers
+    
+    
+    @staticmethod
+    def find_by_name(name,db):
+        c = db.careers.find_one({"name": name})
+        if c:
+            return Career(c["name"])
+        else:
+            return None
