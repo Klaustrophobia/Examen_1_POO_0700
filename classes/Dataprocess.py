b8f27aa4d228bae8b5ab90cdc625a05e96f5755d
Klaustrophobia <Lalosempai2@gmail.com>
2023-02-25 20:39:25 -0600

    ACT II

--- classes/Dataprocess.py
+++ classes/Dataprocess.py
@@ -6,16 +6,19 @@ class Dataprocess:
     def create_careers(self, db):
         ## Do something to create careers on your mongodb collection using __data
         # Crea la coleccion Careers
-        # db.create_collection("Careers")
+        if "Careers" not in db.list_collection_names():
+            db.create_collection("Careers")
 
         #
-        carreras = self.__data[1]['carrera']
-        for career_data in carreras:
-            career_document = {
-                'carrera': career_data['carrera'],
-            }
-            #Inserta el documento en la coleccion Careers
-            db.careers.insert_one(career_document)
+        careers = []
+        for career_data in self.__data['carrera']:
+            careers.append({
+                'carrera': career_data['carrera']
+            })
+        
+        if len(careers) > 0:
+            db.Careers.inser_many(careers)
+
         return True
     def create_courses(self):
         ## Do something to create courses on your mongodb collection using __data
